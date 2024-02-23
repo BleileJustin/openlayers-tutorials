37e584c22a0c757a37974efe03d511a582b16f7a
JustinBleile <justinbleile.dev@gmail.com>
2024-02-23 16:57:13 -0700

    now uses WebGL to improve performance

--- main.js
+++ main.js
@@ -3,14 +3,76 @@ import GeoJSON from 'ol/format/GeoJSON.js';
 import View from 'ol/View.js';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
 import {OSM, Vector as VectorSource} from 'ol/source.js';
+import Layer from 'ol/layer/Layer';
+import WebGLVectorLayerRenderer from 'ol/renderer/webgl/VectorLayer';
 
-const vectorSource = new VectorSource({
+let style;
+
+class WebGLLayer extends Layer {
+  createRenderer() {
+    return new WebGLVectorLayerRenderer(this, {
+      className: this.getClassName(),
+      style,
+    });
+  }
+}
+
+const source = new VectorSource({
   url: './Historic_GeoMAC_Perimeters_2003.geojson',
   format: new GeoJSON(),
 });
 
-const vectorLayer = new VectorLayer({
-  source: vectorSource,
+const getStyle = (dash, pattern) => {
+  let newStyle = {
+    variables: style
+      ? style.variables
+      : {
+          width: 2,
+          offset: 0,
+          capType: 'butt',
+          joinType: 'miter',
+          miterLimit: 10, // ratio
+          dashLength1: 25,
+          dashLength2: 15,
+          dashLength3: 15,
+          dashLength4: 15,
+          dashOffset: 0,
+          patternSpacing: 0,
+        },
+    'stroke-width': ['var', 'width'],
+    'stroke-color': 'red',
+    'stroke-offset': ['var', 'offset'],
+    'stroke-miter-limit': ['var', 'miterLimit'],
+    'stroke-line-cap': ['var', 'capType'],
+    'stroke-line-join': ['var', 'joinType'],
+  };
+  if (dash) {
+    newStyle = {
+      ...newStyle,
+      'stroke-line-dash': [
+        ['var', 'dashLength1'],
+        ['var', 'dashLength2'],
+        ['var', 'dashLength3'],
+        ['var', 'dashLength4'],
+      ],
+      'stroke-line-dash-offset': ['var', 'dashOffset'],
+    };
+  }
+  if (pattern) {
+    delete newStyle['stroke-color'];
+    newStyle = {
+      ...newStyle,
+      'stroke-pattern-src': 'data/dot.svg',
+      'stroke-pattern-spacing': ['var', 'patternSpacing'],
+    };
+  }
+  return newStyle;
+};
+
+style = getStyle(false, false);
+
+const vectorLayer = new WebGLLayer({
+  source,
 });
 
 const map = new Map({
