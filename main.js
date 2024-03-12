9b449d981ff46d55eac69824f5beef41095a4176
JustinBleile <justinbleile.dev@gmail.com>
2024-03-12 06:04:16 -0600

    Added layer switcher control

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+import 'ol-layerswitcher/dist/ol-layerswitcher.css';
+
 import DragAndDrop from 'ol/interaction/DragAndDrop';
 import GeoJSON from 'ol/format/GeoJSON';
 import Link from 'ol/interaction/Link';
 import Map from 'ol/Map';
-import TileLayer from 'ol/layer/WebGLTile'
+import TileLayer from 'ol/layer/WebGLTile';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
 import {OSM} from 'ol/source';
+import LayerSwitcher from 'ol-layerswitcher';
 
 const map = new Map({
   target: 'map-container',
@@ -23,10 +26,22 @@ const map = new Map({
 
 const source = new VectorSource();
 
+const locSource = new VectorSource({
+  url: './data/geojson/world-cities.geojson',
+  format: new GeoJSON(),
+});
+
 const layer = new VectorLayer({
+  title: 'Layer',
   source: source,
 });
 
+const locLayer = new VectorLayer({
+  title: 'Location',
+  source: locSource,
+});
+
+map.addLayer(locLayer);
 map.addLayer(layer);
 
 map.addInteraction(new Link());
@@ -36,3 +51,11 @@ map.addInteraction(
     formatConstructors: [GeoJSON],
   })
 );
+
+const layerSwitcher = new LayerSwitcher({
+  activationMode: 'click',
+  startActive: false,
+  groupSelectStyle: 'children',
+});
+
+map.addControl(layerSwitcher);
